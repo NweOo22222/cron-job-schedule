7fe12e0f78874b08cad2b64e28d6e4b6157f3442
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-23 16:27:12 +0630

    Create stream.js

---
+++ jobs/stream.js
@@ -0,0 +1,32 @@
+module.exports = function stream(q) {
+    let vid, is_live;
+    console.log('> querying "', q, '" at', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+    searchUntilLiveOnYoutube(q)
+        .then(videoId => {
+            vid = videoId;
+            return fetchUntilLiveFromYoutube(vid);
+        })
+        .then(async ({ title, channelName, content, formats }) => {
+            console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+            let format = formats.find(({ qualityLabel }) => qualityLabel === '720p') ||
+                formats.find(({ qualityLabel }) => qualityLabel === '480p') ||
+                formats.find(({ qualityLabel }) => qualityLabel === '360p');
+            let { id, stream_url } = await createLiveStream({
+                title: `${title} - ${channelName}`,
+                description: content,
+            });
+            let { video_id } = await updateLiveStream(id);
+            console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+            is_live = true;
+            broadcastLiveStream(format.url, stream_url);
+        })
+        .catch(async (err) => {
+            console.error(':throw', err);
+            if (is_live) {
+                process.exit(1);
+            }
+            console.log(':set argv.3', vid);
+            console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
+            exec(`node '${__dirname}/../live-stream2.js' '${process.argv[2]}' '${vid}'`);
+        });
+}
