631900c133c5caa1e8f8bd8b06db78c0de011267
htoomyat-pc <htoomyat-pc@localhost>
2021-06-23 18:09:06 +0630

    trying to fix ytdl-core 404 statusa

--- jobs/stream.js
+++ jobs/stream.js
@@ -1,4 +1,28 @@
-module.exports = function stream(q) {
+module.exports = stream;
+
+function __stream() {
+    const { default: axios } = require('axios');
+    const { exec } = require("shelljs");
+    const { searchUntilLiveOnYoutube, broadcastLiveStream } = require("../src/_helpers");
+    const createLiveStream = require("../src/createLiveStream");
+    const updateLiveStream = require("../src/updateLiveStream");
+
+    axios.get('https://www.youtube.com/get_video_info?video_id=qMtcWqCL_UQ&eurl=https%3A%2F%2Fyoutube.googleapis.com%2Fv%2Fonz2k4zoLjQ&html5=1&c=TVHTML5&cver=6.20180913').then(({ data }) => {
+        let u = new URL('http://localhost?' + data);
+        let r = JSON.parse(u.searchParams.get('player_response'));
+        // .streamingData.dashManifestUrl
+    })
+}
+
+function sleep(ms) {
+    return new Promise(res => setTimeout(() => res(), ms));
+}
+
+async function stream(q) {
+    require('../test');
+
+    await sleep(5000);
+
     let vid, is_live;
 
     const { exec } = require("shelljs");
@@ -12,7 +36,7 @@ module.exports = function stream(q) {
             vid = videoId;
             return fetchUntilLiveFromYoutube(vid);
         })
-        .then(async({ title, channelName, content, formats }) => {
+        .then(async ({ title, channelName, content, formats }) => {
             console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }));
             let format = formats.find(({ qualityLabel }) => qualityLabel === '720p') ||
                 formats.find(({ qualityLabel }) => qualityLabel === '480p') ||
@@ -26,7 +50,7 @@ module.exports = function stream(q) {
             is_live = true;
             broadcastLiveStream(format.url, stream_url);
         })
-        .catch(async(err) => {
+        .catch(async (err) => {
             console.error(':throw', err);
             if (is_live) {
                 process.exit(1);
