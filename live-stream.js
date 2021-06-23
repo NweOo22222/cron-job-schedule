9ff8c0ab9a878dbe127323c831c6c6f3bca49b99
htoomyat-pc <htoomyat-pc@localhost>
2021-06-23 17:49:08 +0630

    trying to fix ytdl-core 404 statusa

--- live-stream.js
+++ live-stream.js
@@ -1,3 +1,5 @@
+require('./test');
+
 const { broadcastLiveStream, getVideoInfo } = require('./src/_helpers');
 const createLiveStream = require('./src/createLiveStream');
 const updateLiveStream = require('./src/updateLiveStream');
@@ -5,16 +7,16 @@ const updateLiveStream = require('./src/updateLiveStream');
 const input = process.argv[3];
 
 getVideoInfo(input)
-  .then(async ({ title, channelName, content, formats }) => {
-    if (!formats.length) throw "no video source is available.";
-    let format = formats.find(({ qualityLabel }) => qualityLabel === '720p' || qualityLabel === '480p' || qualityLabel === '360p');
-    let { id, stream_url } = await createLiveStream({
-      title: `${title} - ${channelName}`,
-      description: content,
+    .then(async({ title, channelName, content, formats }) => {
+        if (!formats.length) throw "no video source is available.";
+        let format = formats.find(({ qualityLabel }) => qualityLabel === '720p' || qualityLabel === '480p' || qualityLabel === '360p');
+        let { id, stream_url } = await createLiveStream({
+            title: `${title} - ${channelName}`,
+            description: content,
+        });
+        let { video_id } = await updateLiveStream(id);
+        broadcastLiveStream(format.url, stream_url);
+    }).catch(e => {
+        console.log(e.response ? .headers, e.response ? .data);
+        console.log(e.message);
     });
-    let { video_id } = await updateLiveStream(id);
-    broadcastLiveStream(format.url, stream_url);
-  }).catch(e => {
-    console.log(e.response?.headers, e.response?.data);
-    console.log(e.message);
-  });
