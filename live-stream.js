5dfbc990dbb09fa3acd9588ea9019dd16e483a8c
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-23 19:36:56 +0630

    Update live-stream.js

--- live-stream.js
+++ live-stream.js
@@ -1,23 +1,20 @@
-const getDashUrl = require('./test');
 const input = process.argv[3];
 
-setTimeout(() => {
-    const { broadcastLiveStream, getVideoInfo } = require('./src/_helpers');
-    const createLiveStream = require('./src/createLiveStream');
-    const updateLiveStream = require('./src/updateLiveStream');
+const { broadcastLiveStream, getVideoInfo } = require('./src/_helpers');
+const createLiveStream = require('./src/createLiveStream');
+const updateLiveStream = require('./src/updateLiveStream');
 
-    getVideoInfo(input)
-    .then(async ({ title, channelName, content, formats }) => {
-        if (!formats.length) throw "no video source is available.";
-        let format = formats.find(({ qualityLabel }) => qualityLabel === '720p' || qualityLabel === '480p' || qualityLabel === '360p');
-        let { id, stream_url } = await createLiveStream({
-            title: `${title} - ${channelName}`,
-            description: content,
-        });
-        let { video_id } = await updateLiveStream(id);
-        broadcastLiveStream(format.url, stream_url);
-    }).catch(async e => {
-        console.log(e.response?.headers, e.response?.data);
-        console.log(e.message);
+getVideoInfo(input)
+.then(async ({ title, channelName, content, formats }) => {
+    if (!formats.length) throw "no video source is available.";
+    let format = formats.find(({ qualityLabel }) => qualityLabel === '720p' || qualityLabel === '480p' || qualityLabel === '360p');
+    let { id, stream_url } = await createLiveStream({
+        title: `${title} - ${channelName}`,
+        description: content,
     });
-}, 3000);
+    let { video_id } = await updateLiveStream(id);
+    broadcastLiveStream(format.url, stream_url);
+}).catch(async e => {
+    console.log(e.response?.headers, e.response?.data);
+    console.log(e.message);
+});
