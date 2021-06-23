2ff518f3885837ead6d93f8afc52faf891ba4f2d
Shin Maung Maung <ethereal97@gmail.com>
2021-06-23 09:33:41 +0630

    added live stream method (yt1s)

---
+++ live-stream2.js
@@ -0,0 +1,22 @@
+const Yt1s = require('./lib/Yt1s');
+const createLiveStream = require('./src/createLiveStream');
+const updateLiveStream = require('./src/updateLiveStream');
+
+const input = process.argv[3];
+
+Yt1s
+  .getVideoInfo(input)
+  .then(async ({ q, vid, title, t, a, links, status }) => {
+    if (status !== 'ok') throw new Error('Failed to get Youtube video info');
+    let response = await Yt1s.generateDownloadLink(vid, links.mp4, ['22', '135', '18']);
+    if (response.status !== 'ok') throw new Error('Failed to generate download link');
+    let { id, stream_url } = await createLiveStream({
+      title: `${title} - ${a}`,
+      description: `${title} - ${a}\n\nOriginally uploaded from ${a} on Youtube at https://www.youtube.com/watch?v=${vid}`,
+    });
+    let { video_id } = await updateLiveStream(id);
+    broadcastLiveStream(format.url, stream_url);
+  }).catch(e => {
+    console.log('ERROR::', e.response?.headers, e.response?.data || e.message);
+    console.log(e.message);
+  });
