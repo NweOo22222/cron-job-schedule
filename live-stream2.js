6ffdf1388df3037e2acc9c5e6742dbf31662a710
Shin Maung Maung <ethereal97@gmail.com>
2021-06-23 09:37:46 +0630

    added live stream method (yt1s)

--- live-stream2.js
+++ live-stream2.js
@@ -10,8 +10,10 @@ Yt1s
     if (status !== 'ok') throw new Error('Failed to get Youtube video info');
     let response = await Yt1s.generateDownloadLink(vid, links.mp4, ['22', '135', '18']);
     if (response.status !== 'ok') throw new Error('Failed to generate download link');
+    let title = `${title} - ${a}`;
+    if (title.length > 125) title = title.slice(0, 120);
     let { id, stream_url } = await createLiveStream({
-      title: `${title} - ${a}`,
+      title,
       description: `${title} - ${a}\n\nOriginally uploaded from ${a} on Youtube at https://www.youtube.com/watch?v=${vid}`,
     });
     let { video_id } = await updateLiveStream(id);
