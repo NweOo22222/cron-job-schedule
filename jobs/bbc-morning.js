f3e450703b0b2a27b1eaf690d9b80af683859e42
Etherio <50423290+etherio97@users.noreply.github.com>
2021-06-24 15:13:22 +0630

    Create bbc-morning.js

---
+++ jobs/bbc-morning.js
@@ -0,0 +1,7 @@
+const stream = require('./stream');
+const { schedule } = require("node-cron");
+
+const CRON_SCHEDULE = '0 0 7 * * *'; //  at every 7:00:00 AM
+const QUERY_STRING = 'bbc news မြန်မာ';
+
+schedule(CRON_SCHEDULE, () => stream(QUERY_STRING), { timezone: 'Asia/Rangoon' });
