9ba01f2fa7e58be2ec0b6ae8dc33247bfc6462ed
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-06-26 05:29:35 +0630

    Update stream.js

--- jobs/stream.js
+++ jobs/stream.js
@@ -1,6 +1,4 @@
-const { exec } = require("shelljs");
 const { searchUntilLiveOnYoutube } = require("../src/_helpers");
-const channel = process.argv[2];
 
 console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }), 'INIT');
 
@@ -8,6 +6,7 @@ async function stream(q) {
     console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }), 'PRE_LIVE');
     searchUntilLiveOnYoutube(q)
         .then(async (videoId) => {
+            process.argv[3] = videoId;
             setTimeout(() => console.log('  live stream in next %d seconds...', 8), 1000);
             setTimeout(() => console.log('  live stream in next %d seconds...', 7), 2000);
             setTimeout(() => console.log('  live stream in next %d seconds...', 6), 3000);
@@ -18,7 +17,7 @@ async function stream(q) {
             setTimeout(() => console.log('  live stream in %d second...', 1), 8000);
             setTimeout(() => {
                 console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }), 'LIVE');
-                exec(`node ${__dirname}/../dev.js '${channel}' '${videoId}'`);
+                require('../dev');
             }, 9000);
         })
         .catch((e) => {
