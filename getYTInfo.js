149afc6129f3a145417a3f8378f6cf5b808764fa
Etherio <50423290+etherio97@users.noreply.github.com>
2021-06-29 05:47:37 +0630

    Create getYTInfo.js

---
+++ getYTInfo.js
@@ -0,0 +1,25 @@
+const { default: axios } = require('axios');
+
+function getYTInfo(id) {
+  const url = 'https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8';
+  const options = {
+    url,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      content: {
+        client: {
+          clientName: 'WEB',
+          clientVersion: '2.20210623.00.00'
+        }
+      },
+      videoId: id
+    }
+  };
+  
+  return axios(options).then(({ data }) => data);  
+}
+
+module.exports = getYTInfo;
