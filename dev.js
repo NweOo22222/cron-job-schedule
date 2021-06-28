91c3e726f2de8efc8aa9ad1409bb67b66774d4ca
Etherio <50423290+etherio97@users.noreply.github.com>
2021-06-29 05:51:29 +0630

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,13 +1,16 @@
 let liveId;
 const vid = process.argv[3];
-const user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
-const { default: axios } = require('axios');
+const getYTInfo = require('./getYTInfo');
 const { toUnicode, broadcastLiveStream, createLiveStream, deleteLiveStream, updateLiveStream } = require('./src/_helpers');
+
+/*
+const { default: axios } = require('axios');
 const config = {
     headers: {
         'user-agent': user_agent,
     }
 };
+const user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
 const c = 'TVHTML5'
 const cver = '6.20180913'
 const eurl = 'https://youtube.googleapis.com/v/onz2k4zoLjQ';
@@ -18,18 +21,20 @@ url.searchParams.append('eurl', eurl);
 url.searchParams.append('html5', '1');
 url.searchParams.append('c', c);
 url.searchParams.append('cver', cver);
+*/
 
-axios
-    .get(url.toString(), config)
-    .then(({ data }) => JSON.parse(new URLSearchParams(data).get('player_response')))
+getYTInfo(vid)
     .then(({ videoDetails, streamingData }) => {
         let source_url;
-        const { formats, dashManifestUrl } = streamingData;
+        const { formats, dashManifestUrl, hlsManifestUrl } = streamingData;
         const { title, shortDescription, author } = videoDetails;
         const video_url = `https://www.youtube.com/watch?v=${vid}`;
         const description = `"${toUnicode(title)}" - ${author}\n\n${toUnicode(shortDescription)}\n\nOriginally uploaded from ${author} at ${video_url}\n#NweOoBot #NweOoLive`;
-        if (dashManifestUrl) {
-            console.log('[INFO] live streaming...');
+        if (hlsManifestUrl) {
+            console.log('[INFO] live streaming from HLS...', hlsManifestUrl);
+            source_url = hlsManifestUrl;
+        } else if (dashManifestUrl) {
+            console.log('[INFO] live streaming from DASH...', dashManifestUrl);
             source_url = dashManifestUrl;
         } else {
             let selectedFormat = formats.find(({ qualityLabel }) => qualityLabel === '720p')
