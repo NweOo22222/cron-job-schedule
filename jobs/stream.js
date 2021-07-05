53f5ba552f4c717a57350ad6aa9431658a16624f
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-07-05 21:09:53 +0630

    Update stream.js

--- jobs/stream.js
+++ jobs/stream.js
@@ -7,18 +7,18 @@ async function stream(q) {
     searchUntilLiveOnYoutube(q)
         .then(async (videoId) => {
             process.argv[3] = videoId;
-            setTimeout(() => console.log('  live stream in next %d seconds...', 8), 1000);
-            setTimeout(() => console.log('  live stream in next %d seconds...', 7), 2000);
-            setTimeout(() => console.log('  live stream in next %d seconds...', 6), 3000);
-            setTimeout(() => console.log('  live stream in next %d seconds...', 5), 4000);
-            setTimeout(() => console.log('  live stream in next %d seconds...', 4), 5000);
-            setTimeout(() => console.log('  live stream in %d seconds...', 3), 6000);
-            setTimeout(() => console.log('  live stream in %d seconds...', 2), 7000);
-            setTimeout(() => console.log('  live stream in %d second...', 1), 8000);
+//             setTimeout(() => console.log('  live stream in next %d seconds...', 8), 1000);
+//             setTimeout(() => console.log('  live stream in next %d seconds...', 7), 2000);
+//             setTimeout(() => console.log('  live stream in next %d seconds...', 6), 3000);
+//             setTimeout(() => console.log('  live stream in next %d seconds...', 5), 4000);
+//             setTimeout(() => console.log('  live stream in next %d seconds...', 4), 5000);
+            setTimeout(() => console.log('  live stream in %d seconds...', 3), 1000);
+            setTimeout(() => console.log('  live stream in %d seconds...', 2), 2000);
+            setTimeout(() => console.log('  live stream in %d second...', 1), 3000);
             setTimeout(() => {
                 console.log(' >>', new Date().toLocaleString('en-US', { timeZone: 'Asia/Yangon' }), 'LIVE');
                 require('../dev');
-            }, 9000);
+            }, 4000);
         })
         .catch((e) => {
             console.log('[ERROR]', e);
