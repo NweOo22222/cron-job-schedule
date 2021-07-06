d6769d6d882e3852642a0edcd061c92103cfdd3b
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-07-07 00:35:02 +0630

    Update voa-morning.js

--- jobs/voa-morning.js
+++ jobs/voa-morning.js
@@ -1,7 +1,7 @@
 const stream = require("./stream");
 const { schedule } = require("node-cron");
 
-const CRON_SCHEDULE = '55 59 5 * * *'; // at every 5:59:54 AM
+const CRON_SCHEDULE = '0 0 6 * * *'; // at every 6:00:00 AM
 const QUERY_STRING = 'ဗီြအိုေအ ျမန္မာနံနက္ခင္း';
 
 schedule(CRON_SCHEDULE, () => stream(QUERY_STRING), { timezone: 'Asia/Rangoon' });
