0eee324cde4ad9e7a00ec7f69d952c9adf450b05
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-07-07 00:34:30 +0630

    Update bbc-night.js

--- jobs/bbc-night.js
+++ jobs/bbc-night.js
@@ -2,6 +2,6 @@ const stream = require('./stream');
 const { schedule } = require("node-cron");
 
 const CRON_SCHEDULE = '00 45 20 * * *'; //  at every 8:45:00 PM
-const QUERY_STRING = 'bbc+burmese';
+const QUERY_STRING = 'UCd9maKo3B6jX8pCPzLa2hvA';
 
 schedule(CRON_SCHEDULE, () => stream(QUERY_STRING), { timezone: 'Asia/Rangoon' });
