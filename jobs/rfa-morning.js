384f7152a13170eefd70052eebb6d54132f110ff
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-07-07 00:32:45 +0630

    Update rfa-morning.js

--- jobs/rfa-morning.js
+++ jobs/rfa-morning.js
@@ -2,6 +2,6 @@ const stream = require('./stream');
 const { schedule } = require("node-cron");
 
 const CRON_SCHEDULE = '0 0 7 *  * *'; // at every 7:00:00 AM
-const QUERY_STRING = 'RFA+Burmese';
+const QUERY_STRING = 'RFA နေ့စဉ်တိုက်ရိုက်ထုတ်လွှင့်ချက်';
 
 schedule(CRON_SCHEDULE, () => stream(QUERY_STRING), { timezone: 'Asia/Rangoon' });
