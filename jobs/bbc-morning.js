b75e7e244bc965ef41cb5c4247a035950e61d897
htoomyat1991998 <84788510+htoomyat1991998@users.noreply.github.com>
2021-07-07 01:08:19 +0630

    Update bbc-morning.js

--- jobs/bbc-morning.js
+++ jobs/bbc-morning.js
@@ -2,6 +2,6 @@ const stream = require('./stream');
 const { schedule } = require("node-cron");
 
 const CRON_SCHEDULE = '0 0 7 * * *'; //  at every 7:00:00 AM
-const QUERY_STRING = 'bbc news မြန်မာ';
+const QUERY_STRING = 'BBC News မြန်မာ';
 
 schedule(CRON_SCHEDULE, () => stream(QUERY_STRING), { timezone: 'Asia/Rangoon' });
